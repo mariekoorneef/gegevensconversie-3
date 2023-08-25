629cc3828089afaf16e721a769c01acb1f546e68
marie.koorneef <mariekoorneef@mariekoneefsmbp.kpn>
2023-08-25 18:30:51 +0200

    theme app does not change?

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const theme = extendTheme({
+  config: { initialColorMode: "light", useSystemColorMode: false },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
